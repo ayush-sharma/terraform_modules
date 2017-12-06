75d9a3024adb29f18a8f7aed88c5ac34576ec69f
ayushsharma <ayushsharma.22@gmail.com>
2017-12-06 16:52:19 +0530

    Adding HTTPS port.

--- aws/security_group/http_elb/main.tf
+++ aws/security_group/http_elb/main.tf
@@ -13,6 +13,16 @@ resource "aws_security_group" "security_group" {
   }
 
   ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  }
+
+  ingress {
     from_port = 0
     to_port   = 65535
     protocol  = "tcp"
